93c23a2131a6a1d669b32995be751b75d29172d9
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-14 22:40:33 +0300

    Trainings-2 Посчитать кол-во прожитого времени

--- src/ru/vachok/life/Vachok.java
+++ src/ru/vachok/life/Vachok.java
@@ -11,16 +11,16 @@ public class Vachok {
 
     public static void main( String args ) {
         double currentTime = new Date().getTime();
-        double futureTime = new Date(17, 12, 14, 22, 26).getTime();
-        for (double i = futureTime; i < currentTime; i++) {
+        double futureTime = new Date(17 , 12 , 14 , 22 , 26).getTime();
+        for (int i = 0; i < 100000; i++) {
             double curTime = new Date().getTime();
-        double birth = new Date(84, 1, 7, 2, 0).getTime();
-        double mSec = (curTime - birth);
-        double dateBi = (mSec / 1000 / 60/60/24);
-        String myName = args;
+            double birth = new Date(84 , 1 , 7 , 2 , 0).getTime();
+            double mSec = (curTime - birth);
+            double dateBi = (mSec / 1000 / 60 / 60 / 24);
+            String myName = args;
             System.out.println(myName + " прожил " + dateBi + " days");
         }
-        }
+    }
 }
 
 
