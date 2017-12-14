f3e430a578e2a8d4679a6dd48708efe3dcc4e750
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-14 17:33:56 +0300

    Имя

--- src/ru/vachok/life/Persons.java
+++ src/ru/vachok/life/Persons.java
@@ -9,7 +9,6 @@ import java.util.Scanner;
 
 
 public class Persons {
-   private int h;
    public static void main(String[] args) {
       long todayLong = new Date().getTime();
       System.out.println("Сегодня - "+todayLong+" мсек от рождества Юниксова...");
@@ -17,21 +16,20 @@ public class Persons {
       Scanner enteredAge = new Scanner(System.in);
       int yearBirthEntered = enteredAge.nextInt();
       int age = getAge(yearBirthEntered);
-      if(age < 100) { System.out.println("Ваш возраст = "+age); }
+      if(age < 100) { }
       else {
          System.out.println("ой, не пизди!");
          System.exit(age);
       }
-      System.exit(0);
       if(age > 0) { System.out.println("Ваш возраст = "+age); }
       else {
          System.out.println("ой, не пизди!");
          System.exit(age);
       }
-      System.exit(0);
-
-
-
+      Scanner enterName = new Scanner(System.in);
+      String name = enterName.nextLine();
+      if(name.equals("Vachok")) { Vachok.main(name); }
+      else { System.exit(11); }
    }
    private static int getAge(int yearBirthEntered) {
       int year = Calendar.getInstance().get(Calendar.YEAR);
@@ -41,7 +39,14 @@ public class Persons {
 }
 
 class Vachok {
-   int age;
-   int birthday;
+   public static void main(String args) {
+      String myName = args;
+      System.out.println(myName);
+      long currentDate = new Date().getTime();
+      long birthTime = new Date(1984, 1, 7, 2, 0, 0).getTime();
+      long lifeTime = birthTime-currentDate;
+      // Calendar oldest = new Calendar().get();
+      //  System.out.println(oldest);
+   }
 }
 // at 14.12.2017 (14:42)
