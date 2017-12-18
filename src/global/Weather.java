db42a34a77f52fab0cb39f823735a7e184b2aaa5
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-18 23:21:52 +0300

    Добавить элементы в коллекцию.

--- src/global/Weather.java
+++ src/global/Weather.java
@@ -4,6 +4,23 @@ package global;
 
 public class Weather {
 
+import ru.vachok.life.Main;
+
+
+
+    public class Weather extends Thread {
+
+        @Override
+        public void run() {
+            Thread.currentThread().setName("Weather");
+            Thread.currentThread().checkAccess();
+            boolean alive = Main.currentThread().isAlive();
+            while (alive) {
+                long f = 0;
+                f++;
+            }
+            System.out.println("Weather nested");
+        }
 }
 
 
