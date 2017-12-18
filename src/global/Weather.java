bc3b8f4a728ef08d929b3444c83e78959ce95b0f
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-18 23:50:19 +0300

    gen

---
+++ src/global/Weather.java
@@ -0,0 +1,29 @@
+package global;
+
+
+
+public class Weather {
+
+import ru.vachok.life.Main;
+
+
+
+    public class Weather extends Thread {
+
+        @Override
+        public void run() {
+            Thread.currentThread().setName("Weather");
+            Thread.currentThread().checkAccess();
+            boolean alive = Main.currentThread().isAlive();
+            while (alive) {
+                long f = 0;
+                f++;
+            }
+            System.out.println("Weather nested");
+        }
+    }
+
+
+
+
+//18.12.2017(22:31)by{Vachok}
