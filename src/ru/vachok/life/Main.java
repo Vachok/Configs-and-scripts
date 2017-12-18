4087132dc96b63cc1887917dfa2613bab1ae7fa6
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-18 22:51:14 +0300

    Trainings-19 Прогноз погоды!

--- src/ru/vachok/life/Main.java
+++ src/ru/vachok/life/Main.java
@@ -4,27 +4,12 @@ package ru.vachok.life;
 
 import java.util.Calendar;
 import java.util.Scanner;
-import java.util.concurrent.locks.Lock;
 
 
 
 public class Main extends Thread {
 
     public static String nameChara;
-    static Lock lock = new Lock() {
-        @Override
-        public void lock() {
-
-        }
-
-
-        @Override
-        public void unlock() {
-
-        }
-    };
-
-
 
     public static void main( String... args ) {
         String today = Calendar.getInstance().getTime().toString();
@@ -32,12 +17,10 @@ public class Main extends Thread {
         System.out.println("Введите имя:");
         nameChara = new Scanner(System.in).nextLine();
         Thread person = new Person();
-        lock.lock();
         person.run();
         person.setName("persons-thr");
     }
 
     }
 
-}
 // at 14.12.2017 (14:26)
